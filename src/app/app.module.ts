261d6e228f0469ee971857aa2a69ae29c6f46fd7
igorgbp <igorgabriel12bq@gmail.com>
2023-05-15 01:42:11 -0300

    fixes header, slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatIconModule} from '@angular/material/icon'; 
+import { SliderComponent } from './components/slider/slider.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    SliderComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
